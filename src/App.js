bd4c55b2cb4025d9cdc07fa23234e77fd4493e62
Carlos Delao Martinez <delao.carlos@gmail.com>
2020-07-28 15:56:13 -0700

    Setup dependencies, first commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          test
           Edit <code>src/App.js</code> <br />
           have fun and be creative!
         </p>
